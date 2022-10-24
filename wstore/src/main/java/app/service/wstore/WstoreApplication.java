91df8e3502440ce97a2ab3fc22e0da640dae75c9
huudangnguyen <nguyenhuudang0206@gmail.com>
2022-10-24 16:03:44 +0700

    init project

---
+++ wstore/src/main/java/app/service/wstore/WstoreApplication.java
@@ -0,0 +1,13 @@
+package app.service.wstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstoreApplication.class, args);
+	}
+
+}
---
+++ wstore/src/test/java/app/service/wstore/WstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.service.wstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
